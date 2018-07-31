bb5597d673834d4b845d501c59c8dcb0accd4937
Robby Rohmantri <robbyx273@gmail.com>
2018-07-31 10:40:12 +0700

    update

--- Sample/Controllers/HomeController.cs
+++ Sample/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace Sample.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application description page new.";
 
             return View();
         }
